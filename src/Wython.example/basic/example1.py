697b36c9851a8c7014435f765104d4c131cd0954
jrasmusbm <JRasmusBm@gmail.com>
2018-06-01 09:39:53 +0200

    Add types and fix bug with memory of else statements

--- src/Wython.example/basic/example1.py
+++ src/Wython.example/basic/example1.py
@@ -2,8 +2,12 @@ def a():
   print("a")
   print("b")
 if True:
+  a = 10
+  while a > 0:
+    print("Hi!")
+    a = a - 3
   print("True!")
+print("d")
 if False:
   print("False!")
-print("c")
 a()
