2130abec35bedd356301f3aafcbd5430539b149f
jrasmusbm <JRasmusBm@gmail.com>
2018-06-13 21:50:16 +0200

    Add assumption about function and variable types
    Added assumption that functions and variables all are integers, to
    enable use of integer variables.

--- src/Wython.example/basic/example1.py
+++ src/Wython.example/basic/example1.py
@@ -1,5 +1,9 @@
-def factorial(n) -> int:
+def factorial(n: int) -> int:
   if n <= 0:
     return 1
   return n * factorial(n-1)
-print(factorial(5))
+n = 6
+print("Factorial of:")
+print(n)
+print("Equals: ")
+print(factorial(n))
