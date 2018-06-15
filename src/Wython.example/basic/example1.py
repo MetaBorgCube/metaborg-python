507fe38cdfd6a999fbd7360b0cf8b484112228bc
jrasmusbm <JRasmusBm@gmail.com>
2018-06-15 16:41:18 +0200

    Update examples

--- src/Wython.example/basic/example1.py
+++ src/Wython.example/basic/example1.py
@@ -1,16 +1,9 @@
-def factorial(n: int) -> int:
-  if n <= 0:
-    return 1
-  return n * factorial(n-1)
-def printFactorial(n: int):
-  print("The factorial of " + n + " is:  " + factorial(n))
-printFactorial(3*2)
-print(2 + 2)
 def a():
-  print("a")
-  print("b")
+ print("a")
+ print("b")
 if True:
-  print("True!")
+ print("True!")
 if False:
-  print("False!")
+ print("False!")
 print("c")
+a()
--- src/Wython.example/functions/example3.py
+++ src/Wython.example/functions/example3.py
@@ -1,6 +1,8 @@
-def factorial(value):
-    if value <= 1:
-        return 1
-    return value * factorial(value-1)
-
-print(factorial(12))
+def factorial(n: int) -> int:
+  if n <= 0:
+    return 1
+  return n * factorial(n-1)
+def printFactorial(n: int):
+  print("The factorial of " + n + " is:  " + factorial(n))
+printFactorial(2 * 6)
+print(2 + 2)
