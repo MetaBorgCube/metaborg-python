ce5fd77d49f5e8638ba016151e6c4a8c1660367e
jrasmusbm <JRasmusBm@gmail.com>
2018-06-27 23:35:45 +0200

    Make store and load arrays work

--- src/Wython.example/basic/example1.py
+++ src/Wython.example/basic/example1.py
@@ -1,3 +1,8 @@
-a = [ 20, 15, 32 ]
-a[2] = a[0] + a[1]
-print(a[2])
+#def factorial(n):
+#  if n < 1: 
+#    return 1
+#  return n * factorial(n-1)
+phrases = [2, 4, "I Like Hockey "]
+age = 12 + 12 # factorial(4)
+phrases[2] = phrases[2] + "a lot!"
+print("Rasmus, " + 24 + " years old, said: " + phrases[2])
