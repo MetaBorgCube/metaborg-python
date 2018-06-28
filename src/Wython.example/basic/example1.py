7ab633b54e89f3f00f440e87fceda3267f274c9e
jrasmusbm <JRasmusBm@gmail.com>
2018-06-28 06:36:37 +0200

    It's working!!

--- src/Wython.example/basic/example1.py
+++ src/Wython.example/basic/example1.py
@@ -1,8 +1,8 @@
-#def factorial(n):
-#  if n < 1: 
-#    return 1
-#  return n * factorial(n-1)
+def factorial(n) -> int:
+  if n < 1: 
+    return 1
+  return n * factorial(n-1)
 phrases = [2, 4, "I Like Hockey "]
-age = 12 + 12 # factorial(4)
+age = factorial(4)
 phrases[2] = phrases[2] + "a lot!"
-print("Rasmus, " + 24 + " years old, said: " + phrases[2])
+print("Rasmus, " + age + " years old, said: " + phrases[2])
