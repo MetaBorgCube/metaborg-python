372e1256dbeab253f09ffc820bddca6ec5262c44
jrasmusbm <JRasmusBm@gmail.com>
2018-07-06 09:55:02 +0200

    Demo done!

---
+++ src/Wython.example/demo/printString.py
@@ -0,0 +1,40 @@
+# 1. Print Strings
+
+# print("Hello, fellow Language Engineers!")
+
+# 2. Print Numbers
+
+# print(42)
+
+# 3. Variables
+
+# var = "I am a variable!"
+# print(var)
+
+# 4. Expressions
+
+# print((2 + 2) * 3 - 2)
+
+# 5. Control Statements
+if True:
+  b = "True"
+else:
+  b = False
+print(b) 
+
+# 6. Functions
+
+# def mod(n1, n2) ->  int:
+#   return n1 % n2  
+# print(mod(13, 10))
+
+# 7. String Concatenation
+
+# print("Hello, " + "world!")
+
+# 8. Lists
+
+# print([2, "The string I want", 1][1])
+
+# 9(*). Print Lists
+# 10(*). More extended program
--- src/Wython.example/demo/string-concatenation.py
+++ src/Wython.example/demo/string-concatenation.py
@@ -1,9 +1,1 @@
-def factorial(n) -> int:
-  if n < 1: 
-    return 1
-  return n * factorial(n-1)
-phrases = ["I like Football more!", "I Like Hockey "]
-age = factorial(4)
-phrases[1] = phrases[1] + "a lot!"
-print("Rasmus, " + age + " years old, said: " + phrases[1])
-print("Chiel, " + (factorial(3) + 16) + " years old, said: " + phrases[0])
+a = [ x + y for x, y in pairs ]
